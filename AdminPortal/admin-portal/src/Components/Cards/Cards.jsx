9b6f0eef5483a5bfee88ca00c6c2fbdce31607f9
YashfeenMirza <yashfeenmirza93@gmail.com>
2023-04-13 16:01:46 +0500

    graphs added

--- AdminPortal/admin-portal/src/Components/Cards/Cards.jsx
+++ AdminPortal/admin-portal/src/Components/Cards/Cards.jsx
@@ -1,56 +1,22 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CardsData } from "../Data/Data";
 import './Cards.css'
-import { CardsData } from '../Data/Data'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faClipboardList,
-    faMoneyCheckAlt,
-    faUsers,
-    faChartLine,
-    faPowerOff,
-    faCommentDots,
-    faCheckCircle,
-    faSpinner,
-} from '@fortawesome/free-solid-svg-icons';
-function Cards() {
+const Cards = () => {
   return (
-   <div className="Cards">
-    {/* {CardsData.map((card,id)=>{
-        return(
-            <div className="parentsContainer">
-                <Card
-                title={card.title}
-                color={card.color}
-                barValue={card.barValue}
-                value={card.value}
-                series={card.series}/>
+    <div className="flex div1">
+      {CardsData.map((item, index) => {
+        return (
+          <div className="card Card1">
+            <div className="card-body ">
+              <h5 className="card-title flex">{item.value} <FontAwesomeIcon icon={item.icon}/></h5>
+              <p className="card-text">{item.discription}</p>
             </div>
-        )
-    })} */}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-{/* <div className="card">
-  <div className="card-body">
-    <h5 className="card-title">120</h5>
-    <p className="card-text">New Orders</p>
-  </div>
-</div> */}
-<ul className="box-info">
-    {CardsData.map((item,index)=>{
-        return(
-            <li>
-                <div className='icon'>
-                <FontAwesomeIcon icon={item.icon}/>
-                </div>
-                <span>
-                    <h3>{item.value}</h3>
-                    <p>{item.discription}</p>
-                </span>
-            </li>
-        )
-    })}
-</ul>
-   </div>
-  )
-}
-
-export default Cards
+export default Cards;
--- AdminPortal/admin-portal/src/Components/Dashboard/MainDash.jsx
+++ AdminPortal/admin-portal/src/Components/Dashboard/MainDash.jsx
@@ -1,65 +1,129 @@
-import React from 'react'
-import './MainDash.css'
-import Cards from '../Cards/Cards'
-import DashNav from './DashNav'
+import React from 'react';
+import './MainDash.css';
+import Cards from '../Cards/Cards';
+import DashNav from './DashNav';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import {
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+);
 ChartJS.register(ArcElement, Tooltip, Legend);
 const MainDash = () => {
-  return (
-    <div className="MainDash">
-      <div>
-      <DashNav/>
-      </div>
-        
-        <Cards/>
-        <div className='flex'>
-          <div className='card'>
-            <div className="card-body">
-              <h5 className='card-title'>Transactions</h5>
-              <PieChart1/>
+    return (
+        <div className="MainDash">
+            <div>
+                <DashNav />
+                <Cards />
             </div>
-          
-          </div>
-          <div className='card'>
-            <div className="card-body">
-              <h5 className='card-title'>Transactions</h5>
-              <PieChart1/>
+            <div className="flex">
+                <div className="chart-div">
+                    <TransactionChart1 />
+                </div>
+                <div className="chart-div">
+                    <OrderChart1 />
+                </div>
+                <div className="chart-div">
+                    <OrderChart1 />
+                </div>
             </div>
-          
-          </div>
-        
-          
         </div>
-    </div>
-  )
-}
-function PieChart1(){
-  const data = {
-    labels: ['Successful', 'Pending'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [19, 12],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
+    );
+};
+function OrderChart1() {
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Orders Monthly Growth',
+            },
+        },
+    };
+    const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+    ];
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Therapists',
+                data: [69, 32, 89, 50, 33, 33, 100],
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Physicians',
+                data: [100, 70, 200, 150, 333, 229, 400],
+                borderColor: 'rgb(53, 162, 235)',
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
         ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+    };
 
- 
-
-  return (
-    <div style={{ width: '300px', height: '200px' }}>
-      <Pie data={data}/>
-    </div>
-  );
+    return (
+        <div style={{ width: '24rem', height: '12rem', background: '#DFDDDE' }}>
+            <Line options={options} data={data} />
+        </div>
+    );
 }
-export default MainDash
+function TransactionChart1() {
+    const data = {
+        labels: ['Successful', 'Pending'],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [19, 12],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                ],
+                borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
+                borderWidth: 1,
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Transactions',
+            },
+        },
+    };
+
+    return (
+        <div style={{ width: '270px', height: '200px', background: '#DFDDDE' }}>
+            <Pie data={data} options={options} />
+        </div>
+    );
+}
+
+export default MainDash;
