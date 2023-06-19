0e4cbaf4fba0cbdaceab0773cb57f7a579ecd77f
ameerparial <khuhroameerhamza4@gmail.com>
2023-06-19 16:07:16 +0500

    Necessary fields are added in the profile page.

--- AdminPortal/admin-portal/src/Components/Profile-Form/ProfileFormMain.jsx
+++ AdminPortal/admin-portal/src/Components/Profile-Form/ProfileFormMain.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ProfileFormMain.css';
 function ProfileFormMain() {
+    
+    const [profileData, setProfileData] = useState({
+        name:'',
+        address:'',
+        busines_id:'',
+        contact:''
+
+    })
+    const handleChange = (e)=>{
+        console.log(e);
+        const key = e.target.name;
+        const value = e.target.value;
+        setProfileData({...profileData, [key]:value})
+    }
+
+    const updateData = (e)=>{
+        e.preventDefault();
+        console.log(profileData);
+    }
     return (
         <div className="main-container">
             <div className="container">
@@ -8,33 +27,46 @@ function ProfileFormMain() {
                     <div className="card-title">
                         <h4>Edit Profile</h4>
                     </div>
+
                     <div className="card-body">
                         <form>
+                            <div className="form-group">
+                                <label for="img">Select profile image: </label>
+                                <input type="file" id="img" name="img" accept="image/*"onChange={(e)=>{
+                                    setProfileData({...profileData, image:e.target.files[0]})
+                                }}/>                                    
+                            </div>
                             <div className="form-group">
                                 <label htmlFor="name">Name</label>
                                 <input
                                     type="text"
-                                    className="form-control"
+                                    className="form-control" 
+                                    name='name'
                                     id="name"
-                                    value="Yashfeen Mirza"
+                                    value={profileData.name}
+                                    onChange={handleChange}
                                 />
                             </div>
                             <div className="form-group">
-                                <label htmlFor="email">Email address</label>
+                                <label htmlFor="bus_id">Business ID</label>
                                 <input
                                     type="text"
                                     className="form-control"
-                                    id="email"
-                                    value="yashfeenmirza93@gmail.com"
+                                    id="bus_id"
+                                    name='bussiness_id'
+                                    value={profileData.busines_id}
+                                    onChange={handleChange}
                                 />
                             </div>
                             <div className="form-group">
-                                <label htmlFor="password">Password</label>
+                                <label htmlFor="address">Business Address</label>
                                 <input
-                                    type="password"
+                                    type="text"
                                     className="form-control"
-                                    id="password"
-                                    value="shdbjsbjdsk"
+                                    id="address"
+                                    name='address'
+                                    value={profileData.address}
+                                    onChange={handleChange}
                                 />
                             </div>
                             <div className="form-group">
@@ -43,12 +75,14 @@ function ProfileFormMain() {
                                     type="text"
                                     className="form-control"
                                     id="contact"
-                                    value="03223676099"
+                                    name='contact'
+                                    value={profileData.contact}
+                                    onChange={handleChange}
                                 />
                             </div>
 
-                            <button type="submit" className="btn btn-primary">
-                                Submit
+                            <button type="submit" className="btn btn-primary" onClick={updateData}>
+                                Update Info
                             </button>
                         </form>
                     </div>
