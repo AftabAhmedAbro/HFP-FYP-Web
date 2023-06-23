569e11c3223ae2db85a86d2b0548e6041fccc4c7
ameerparial <khuhroameerhamza4@gmail.com>
2023-06-23 12:06:26 +0500

    Storage Bucket is added in firebase config

--- AdminPortal/admin-portal/src/Components/firebaseAuth.js
+++ AdminPortal/admin-portal/src/Components/firebaseAuth.js
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from "firebase/storage"
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
@@ -20,5 +22,7 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+const storage = getStorage(app);
+const db = getFirestore(app);
 export default firebaseConfig;
-export { auth, app };
+export { auth, app, storage, db};
